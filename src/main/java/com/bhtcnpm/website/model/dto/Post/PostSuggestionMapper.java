61e2f29be4ba1b934c7e0b4405b2adb435a4b07f
mirushi <luubieunghi@gmail.com>
2021-03-22 13:08:51 +0700

    Full-text search added for quick search Post. Closes #43 .

--- src/main/java/com/bhtcnpm/website/model/dto/Post/PostSuggestionMapper.java
+++ src/main/java/com/bhtcnpm/website/model/dto/Post/PostSuggestionMapper.java
@@ -2,11 +2,14 @@ package com.bhtcnpm.website.model.dto.Post;
 
 import com.bhtcnpm.website.model.entity.PostEntities.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper
 public interface PostSuggestionMapper {
+
+    PostSuggestionMapper INSTANCE = Mappers.getMapper(PostSuggestionMapper.class);
 
     PostSuggestionDTO postToPostSuggestionDTO (Post post);
 
