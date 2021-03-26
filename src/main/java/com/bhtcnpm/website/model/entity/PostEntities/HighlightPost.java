391d2a7b28242b72b19267689f3fe1000ce58149
mirushi <luubieunghi@gmail.com>
2021-03-26 18:00:31 +0700

    WIP #2

--- src/main/java/com/bhtcnpm/website/model/entity/PostEntities/HighlightPost.java
+++ src/main/java/com/bhtcnpm/website/model/entity/PostEntities/HighlightPost.java
@@ -1,2 +1,28 @@
-package com.bhtcnpm.website.model.entity.PostEntities;public class HighlightPost {
+package com.bhtcnpm.website.model.entity.PostEntities;
+
+import com.bhtcnpm.website.model.entity.UserWebsite;
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "highlight_post")
+@Data
+public class HighlightPost {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "post")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "highlighted_by_user")
+    private UserWebsite highlightedBy;
+
+    @Column(unique = true)
+    private Integer rank;
+
+    @NotNull
+    private LocalDateTime highlightDtm;
 }
