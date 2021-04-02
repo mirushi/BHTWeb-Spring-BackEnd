9c873d19807ba41c95fd401bb05547a388beb92c
Nghi <luubieunghi@gmail.com>
2021-04-02 23:32:47 +0700

    Fixed get doc statistic throw exception
    Closes #34 .

--- src/main/java/com/bhtcnpm/website/model/dto/Doc/DocCommentStatisticDTO.java
+++ src/main/java/com/bhtcnpm/website/model/dto/Doc/DocCommentStatisticDTO.java
@@ -1,10 +1,12 @@
 package com.bhtcnpm.website.model.dto.Doc;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class DocCommentStatisticDTO {
     private Long docID;
     private Long commentCount;
