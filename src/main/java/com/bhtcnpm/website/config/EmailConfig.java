487faddc018f6c7be3385f6fda2800b5cff392c7
Nghi <luubieunghi@gmail.com>
2021-04-16 06:11:55 +0700

    WIP

---
+++ src/main/java/com/bhtcnpm/website/config/EmailConfig.java
@@ -0,0 +1,17 @@
+package com.bhtcnpm.website.config;
+
+public class EmailConfig {
+    public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
+    public static final String USERNAME = "bht.cnpm.bot@gmail.com";
+    public static final String PASSWORD = "GSj2wLj6dx5p9lOl5J%F9A7UFkJMJ6h*";
+    public static final String FROM_EMAIL = "bht.cnpm.bot@gmail.com";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_SUBJECT = "";
+    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+}
