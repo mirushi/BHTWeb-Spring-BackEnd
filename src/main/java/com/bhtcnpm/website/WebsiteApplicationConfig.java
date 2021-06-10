10fa3869fd728035fe0277c4c47b7aadeaaf99e5
Nghi <luubieunghi@gmail.com>
2021-06-10 08:41:44 +0700

    Fix CORS for token

--- src/main/java/com/bhtcnpm/website/WebsiteApplicationConfig.java
+++ src/main/java/com/bhtcnpm/website/WebsiteApplicationConfig.java
@@ -15,7 +15,7 @@ public class WebsiteApplicationConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "PUT", "POST", "DELETE")
-                        .allowedHeaders("userLoginToken", "Content-Type");
+                        .allowedHeaders("Authorization", "Cache-Control" ,"Content-Type");
             }
         };
     }
